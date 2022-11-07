1b31ac0f49d0257eaaf1a57be5d73b25c71b442c
galvaal <115516730+galvaal@users.noreply.github.com>
2022-11-07 15:52:35 +0700

    Add files via upload

---
+++ Bilangan Terbesar dari 3 buah bilangan.py
@@ -0,0 +1,9 @@
+a=int(input("Masukan bilangan ke-1: "))
+b=int(input("Masukan bilangan ke-2: "))
+c=int(input("Masukan bilangan ke-3: "))
+if (a>b and a>c):
+    print("Hasil bilangan terbesar adalah : ",a)
+elif (b>a and b>c):
+    print("Hasil bilangan terbesar adalah : ",b)
+else :
+    print("hasil bilangan terbesar adalah : ",c)
